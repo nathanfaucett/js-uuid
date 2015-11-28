82d06e5c24d935a1552c21ecec8f28d21a560d93
nathanfaucett <nathanfaucett@gmail.com>
2015-11-28 08:24:22 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 var CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split(""),
-    UUID = new Array(36);
+    UUID_ARRAY = new Array(36);
 
 
 module.exports = uuid;
 
 
 function uuid() {
-    var i = -1;
+    var localRandom = Math.random,
+        localArray = UUID_ARRAY,
+        localChars = CHARS,
+        i = -1;
 
     while (i++ < 36) {
         if (i === 8 || i === 13 || i === 18 || i === 23) {
-            UUID[i] = "-";
+            localArray[i] = "-";
         } else if (i === 14) {
-            UUID[i] = "4";
+            localArray[i] = "4";
         } else {
-            UUID[i] = CHARS[(Math.random() * 62) | 0];
+            localArray[i] = localChars[(localRandom() * 62) | 0];
         }
     }
 
-    return UUID.join("");
+    return localArray.join("");
 }
